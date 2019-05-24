b6aa25fff3436b9983b7e6cd3a82617c0420e179
Mohammad Tourjoman <mtourjoman01@hotmail.com>
2019-05-24 14:06:02 -0700

    Completed calculator project MVP

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app">
+      <CalculatorDisplay />
+      <div className='row'>
+        <NumberButton value="clear" />
+        <ActionButton value="&divide;" />
+      </div>
+      <div className='row'>
+        <NumberButton value="7" />
+        <NumberButton value="8" />
+        <NumberButton value="9" />
+        <ActionButton value="&times;" />
+      </div>
+      <div className='row'>
+        <NumberButton value="4" />
+        <NumberButton value="5" />
+        <NumberButton value="6" />
+        <ActionButton value="&minus;" />
+      </div>
+      <div className='row'>
+        <NumberButton value="1" />
+        <NumberButton value="2" />
+        <NumberButton value="3" />
+        <ActionButton value="+" />
+      </div>
+      <div className='row'>
+        <NumberButton value="0" />
+        <ActionButton value="=" />
+      </div>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+  return (
+    <a href="##" className="button actionBtn">{props.value}</a>
+  );
+}
+
+export default ActionButton;
